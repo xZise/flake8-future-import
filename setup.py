86aa7869cc80ac8c34bf74e24ba02935ac400007
Fabian Neundorf <CommodoreFabianus@gmx.de>
2015-10-18 23:12:25 +0000

    [IMPROV] Install flake8 if it's going to be tested
    When TEST_FLAKE8_INSTALL is set, it'll always test it using flake8. Either it's
    locally installed or it'll install it in the test. In that case it can install
    flake8 beforehand. If the plugin is already installed, this won't change
    anything as that has already installed flake8.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import codecs
+import os
 
 from setuptools import setup
 
@@ -14,6 +15,15 @@ def get_version():
 def get_long_description():
     with codecs.open('README.rst', 'r', 'utf-8') as f:
         return f.read()
+
+
+# Install flake8 if that is set as we are going to test with flake8 as well
+if os.environ.get('TEST_FLAKE8_INSTALL') == '1':
+    tests_require = ['flake8']
+else:
+    tests_require = []
+
+tests_require += ['six']
 
 
 setup(
@@ -34,7 +44,7 @@ setup(
             'flake8-future-import = flake8_future_import:FutureImportChecker',
         ],
     },
-    tests_require=['six'],
+    tests_require=tests_require,
     test_suite='test_flake8_future_import',
     classifiers=[
         'Development Status :: 3 - Alpha',
